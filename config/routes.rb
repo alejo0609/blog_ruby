9957ae43c07632b86824cf9a92a0274b55b000f0
alejo0609 <alejandro.perez9@udea.edu.co>
2022-04-07 23:57:06 -0500

    Cambios de devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'welcome/index'
 
   resources :articles
-
   root "articles#index"
   # Cambiar en caso tal por esta: 
   # root 'welcome#index'
